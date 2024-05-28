8d0741fdb92136ac3a661de35b4c0fc6fe9368d8
Thorsten Staerk <thorstenstaerk@google.com>
2024-05-28 23:11:01 +0200

    Update describepic.py
    allow png and jpg files

--- describepic.py
+++ describepic.py
@@ -3,7 +3,7 @@ import sys
 import base64
 from vertexai.preview.generative_models import GenerativeModel, Part
 
-# This will take the png file handed over as first arguement and have AI describe it
+# This will take the image file handed over as first arguement and have AI describe it
 
 API_KEY = 'AIreplacebyyoursV0'
 
@@ -12,8 +12,14 @@ ai.configure(api_key=API_KEY)
 model = GenerativeModel("gemini-pro-vision")
 
 imagefile=sys.argv[1]
+if (imagefile[-3:]=="jpg"):
+    imagetype="jpeg"
+if (imagefile[-3:]=="png"):
+    imagetype="png"
+
 filehandle=open(imagefile,"rb")
 base64_image=base64.b64encode(filehandle.read())
-image0=Part.from_data(data=base64.b64decode(base64_image), mime_type="image/png")
+image0=Part.from_data(data=base64.b64decode(base64_image), mime_type="image/"+imagetype)
 
 print(model.generate_content(["can you describe this picture?",image0]).candidates[0].content.parts[0].text)
+
