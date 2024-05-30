bfc1643bf62e4aba9e654fb4f26e08af69d43b50
Thorsten Staerk <thorstenstaerk@google.com>
2024-05-30 09:21:23 +0200

    Create joke.py

---
+++ joke.py
@@ -0,0 +1,12 @@
+import google.generativeai as ai
+
+# This program lets AI tell a joke
+# It is useful not only to put a smile on your face, but also to check if you have set up libraries and API keys correctly
+
+API_KEY = 'APIKEY'
+
+ai.configure(api_key=API_KEY)
+
+model = ai.GenerativeModel("gemini-pro")
+
+print(model.generate_content("tell a joke").candidates[0].content.parts[0].text)
